986b5b092251cf954cbb787f12dec8562e1daf8f
psbrunosouza <psbrunosouza@gmail.com>
2023-08-20 11:45:53 -0300

    feat: retornando o valor IsLoop da iteration

--- internal/entities/iteration.entity.go
+++ internal/entities/iteration.entity.go
@@ -2,6 +2,6 @@ package entities
 
 type Iteration struct {
 	Default
-	IsLoop bool   `json:"isLoop,omitempty" gorm:"default:false;"`
+	IsLoop bool   `json:"isLoop" gorm:"default:false;"`
 	Tasks  []Task `json:"tasks,omitempty"`
 }
