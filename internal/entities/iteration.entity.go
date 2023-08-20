a07a34861eaa0fdf4ff1c59207a772433ad0c941
psbrunosouza <psbrunosouza@gmail.com>
2023-08-20 11:44:00 -0300

    feat: implementado deleção em cascata das tasks

--- internal/entities/iteration.entity.go
+++ internal/entities/iteration.entity.go
@@ -3,5 +3,5 @@ package entities
 type Iteration struct {
 	Default
 	IsLoop bool   `json:"isLoop,omitempty" gorm:"default:false;"`
-	Tasks  []Task `json:"tasks,omitempty" gorm:"foreignKey:IterationID;constraint:OnDelete:CASCADE"`
+	Tasks  []Task `json:"tasks,omitempty"`
 }
